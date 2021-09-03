f96f93b26e57c59883acfe81182f0db36faa5784
jinayshah <jinay.sh99@gmail.com>
2021-09-03 16:03:58 -0400

    remove extra code

--- services/linkgraph/main.go
+++ services/linkgraph/main.go
@@ -10,14 +10,10 @@ import (
 	"github.com/jinayshah7/distributedSearchEngine/services/linkgraph/graph"
 	"github.com/jinayshah7/distributedSearchEngine/services/linkgraph/linkgraphapi"
 	"github.com/jinayshah7/distributedSearchEngine/services/linkgraph/linkgraphapi/proto"
-	"github.com/urfave/cli"
 	"google.golang.org/grpc"
 )
 
 func main() {
-}
-
-func runMain(appCtx *cli.Context) error {
 	var wg sync.WaitGroup
 	grpcPort := os.GetEnv("GRPC_PORT")
 	linkGraphURI := os.GetEnv("LINK_GRAPH_URI")
@@ -36,13 +32,12 @@ func runMain(appCtx *cli.Context) error {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		srv := grpc.NewServer()
+		grpcServer := grpc.NewServer()
 		proto.RegisterLinkGraphServer(srv, linkgraphapi.NewLinkGraphServer(graph))
-		_ = srv.Serve(grpcListener)
+		_ = grpcServer.Serve(grpcListener)
 	}()
 
 	wg.Wait()
-	return nil
 }
 
 func getLinkGraph(linkGraphURI string) (graph.Graph, error) {
