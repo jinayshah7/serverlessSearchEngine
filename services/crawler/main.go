5c85a698af9ff9ae7f3602d02d6583eb5530a7f0
jinayshah <jinay.sh99@gmail.com>
2021-09-16 17:16:42 -0400

    refactor main.go for crawler

--- services/crawler/main.go
+++ services/crawler/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"net"
-	"net/http"
-	_ "net/http/pprof"
+	"errors"
+	"log"
 	"os"
 	"os/signal"
-	"runtime"
 	"strings"
 	"sync"
 	"syscall"
@@ -26,86 +23,14 @@ import (
 	"google.golang.org/grpc"
 )
 
-var (
-	appName = "services-crawler"
-	appSha  = "populated-at-link-time"
-	logger  *logrus.Entry
-)
-
 func main() {
-	host, _ := os.Hostname()
-	rootLogger := logrus.New()
-	rootLogger.SetFormatter(new(logrus.JSONFormatter))
-	logger = rootLogger.WithFields(logrus.Fields{
-		"app":  appName,
-		"sha":  appSha,
-		"host": host,
-	})
-
-	if err := makeApp().Run(os.Args); err != nil {
-		logger.WithField("err", err).Error("shutting down due to error")
-		_ = os.Stderr.Sync()
-		os.Exit(1)
-	}
-}
-
-func makeApp() *cli.App {
-	app := cli.NewApp()
-	app.Name = appName
-	app.Version = appSha
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:   "link-graph-api",
-			EnvVar: "LINK_GRAPH_API",
-			Usage:  "The gRPC endpoint for connecting to the link graph",
-		},
-		cli.StringFlag{
-			Name:   "text-indexer-api",
-			EnvVar: "TEXT_INDEXER_API",
-			Usage:  "The gRPC endpoint for connecting to the text indexer",
-		},
-		cli.IntFlag{
-			Name:   "num-workers",
-			Value:  runtime.NumCPU(),
-			EnvVar: "NUM_WORKERS",
-			Usage:  "The number of workers to use for crawling web-pages",
-		},
-		cli.DurationFlag{
-			Name:   "update-interval",
-			Value:  5 * time.Minute,
-			EnvVar: "UPDATE_INTERVAL",
-			Usage:  "The time between subsequent crawler runs",
-		},
-		cli.DurationFlag{
-			Name:   "reindex-threshold",
-			Value:  7 * 24 * time.Hour,
-			EnvVar: "REINDEX_THRESHOLD",
-			Usage:  "The minimum amount of time before re-indexing an already-crawled link",
-		},
-		cli.StringFlag{
-			Name:   "partition-detection-mode",
-			Value:  "single",
-			EnvVar: "PARTITION_DETECTION_MODE",
-			Usage:  "The partition detection mode to use. Supported values are 'dns=HEADLESS_SERVICE_NAME' (k8s) and 'single' (local dev mode)",
-		},
-		cli.IntFlag{
-			Name:   "pprof-port",
-			Value:  6060,
-			EnvVar: "PPROF_PORT",
-			Usage:  "The port for exposing pprof endpoints",
-		},
-	}
-	app.Action = runMain
-	return app
-}
-
-func runMain(appCtx *cli.Context) error {
 	var wg sync.WaitGroup
-	ctx, cancelFn := context.WithCancel(context.Background())
-	defer cancelFn()
 
-	// Start crawler
-	partDet, err := getPartitionDetector(appCtx.String("partition-detection-mode"))
+	linkGraphURL := os.GetEnv("LINK_GRAPH_URL")
+	textIndexerURL := os.GetEnv("TEXT_INDEXER_URL")
+	partitionDetectionMode := os.GetEnv("PARITION_DETECTION_MODE")
+
+	partitionDetector, err := getPartitionDetector(appCtx.String("partition-detection-mode"))
 	if err != nil {
 		return err
 	}
@@ -115,15 +40,15 @@ func runMain(appCtx *cli.Context) error {
 		return err
 	}
 
-	var crawlerCfg crawler.Config
-	crawlerCfg.FetchWorkers = appCtx.Int("num-workers")
-	crawlerCfg.UpdateInterval = appCtx.Duration("update-interval")
-	crawlerCfg.ReIndexThreshold = appCtx.Duration("reindex-threshold")
-	crawlerCfg.GraphAPI = graphAPI
-	crawlerCfg.IndexAPI = indexerAPI
-	crawlerCfg.PartitionDetector = partDet
-	crawlerCfg.Logger = logger
-	crawlSvc, err := crawler.NewService(crawlerCfg)
+	crawlerConfig := crawler.Config{
+		FetchWorkers:      os.GetEnv("NUM_WORKERS"),
+		UpdateInterval:    os.GetEnv("UPDATE_INTERVAL"),
+		ReIndexThreshold:  os.GetEnv("RE_INDEX_THRESHOLD"),
+		GraphAPI:          graphAPI,
+		IndexAPI:          indexerAPI,
+		PartitionDetector: partitionDetector,
+	}
+	crawlerService, err := crawler.NewService(crawlerConfig)
 	if err != nil {
 		return err
 	}
@@ -131,70 +56,45 @@ func runMain(appCtx *cli.Context) error {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		if err := crawlSvc.Run(ctx); err != nil {
-			logger.WithField("err", err).Error("crawler service exited with error")
-			cancelFn()
+		if err := crawlerService.Run(ctx); err != nil {
+			log.Fatalf("err", err).Error("crawler service exited with error")
 		}
 	}()
 
-	// Start pprof server
-	pprofListener, err := net.Listen("tcp", fmt.Sprintf(":%d", appCtx.Int("pprof-port")))
-	if err != nil {
-		return err
-	}
-	defer func() { _ = pprofListener.Close() }()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		logger.WithField("port", appCtx.Int("pprof-port")).Info("listening for pprof requests")
-		srv := new(http.Server)
-		_ = srv.Serve(pprofListener)
-	}()
-
-	// Start signal watcher
-	go func() {
-		sigCh := make(chan os.Signal, 1)
-		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGHUP)
-		select {
-		case s := <-sigCh:
-			logger.WithField("signal", s.String()).Infof("shutting down due to signal")
-			_ = pprofListener.Close()
-			cancelFn()
-		case <-ctx.Done():
-		}
-	}()
-
-	// Keep running until we receive a signal
 	wg.Wait()
-	return nil
 }
 
 func getAPIs(ctx context.Context, linkGraphAPI, textIndexerAPI string) (*linkgraphapi.LinkGraphClient, *textindexerapi.TextIndexerClient, error) {
+
 	if linkGraphAPI == "" {
-		return nil, nil, xerrors.Errorf("link graph API must be specified with --link-graph-api")
+		return nil, nil, errors.New("link graph API must be specified with --link-graph-api")
 	}
+
 	if textIndexerAPI == "" {
-		return nil, nil, xerrors.Errorf("text indexer API must be specified with --text-indexer-api")
+		return nil, nil, errors.New("text indexer API must be specified with --text-indexer-api")
 	}
 
 	dialCtx, cancelFn := context.WithTimeout(ctx, 5*time.Second)
 	defer cancelFn()
-	linkGraphConn, err := grpc.DialContext(dialCtx, linkGraphAPI, grpc.WithInsecure(), grpc.WithBlock())
+
+	linkGraphConnection, err := grpc.DialContext(dialCtx, linkGraphAPI, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
-		return nil, nil, xerrors.Errorf("could not connect to link graph API: %w", err)
+		return nil, nil, errors.New("could not connect to link graph API: %w", err)
 	}
-	graphCli := linkgraphapi.NewLinkGraphClient(ctx, linkgraphproto.NewLinkGraphClient(linkGraphConn))
+
+	graphClient := linkgraphapi.NewLinkGraphClient(ctx, linkgraphproto.NewLinkGraphClient(linkGraphConnection))
 
 	dialCtx, cancelFn = context.WithTimeout(ctx, 5*time.Second)
 	defer cancelFn()
-	indexerConn, err := grpc.DialContext(dialCtx, textIndexerAPI, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil {
-		return nil, nil, xerrors.Errorf("could not connect to text indexer API: %w", err)
-	}
-	indexerCli := textindexerapi.NewTextIndexerClient(ctx, textindexerproto.NewTextIndexerClient(indexerConn))
 
-	return graphCli, indexerCli, nil
+	indexerConnection, err := grpc.DialContext(dialCtx, textIndexerAPI, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return nil, nil, errors.New("could not connect to text indexer API: %w", err)
+	}
+
+	indexerClient := textindexerapi.NewTextIndexerClient(ctx, textindexerproto.NewTextIndexerClient(indexerConnection))
+
+	return graphClient, indexerClient, nil
 }
 
 func getPartitionDetector(mode string) (partition.Detector, error) {
