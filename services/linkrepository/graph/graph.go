611efb9a56b59d3c6062ec73ed5f7d96f8f3b574
jinayshah <jinay.sh99@gmail.com>
2021-09-23 08:05:06 -0400

    refactor proto files

--- services/linkrepository/graph/graph.go
+++ services/linkrepository/graph/graph.go
@@ -6,22 +6,6 @@ import (
 	"github.com/google/uuid"
 )
 
-type Iterator interface {
-	Next() bool
-	Error() error
-	Close() error
-}
-
-type LinkIterator interface {
-	Iterator
-	Link() *Link
-}
-
-type EdgeIterator interface {
-	Iterator
-	Edge() *Edge
-}
-
 type Link struct {
 	ID          uuid.UUID
 	URL         string
@@ -39,8 +23,5 @@ type Edge struct {
 type Graph interface {
 	SaveLink(link *Link) error
 	SaveEdge(edge *Edge) error
-	FindLink(id uuid.UUID) (*Link, error)
-	Links(fromID, toID uuid.UUID, retrievedBefore time.Time) (LinkIterator, error)
-	Edges(fromID, toID uuid.UUID, updatedBefore time.Time) (EdgeIterator, error)
 	RemoveOldEdges(fromID uuid.UUID, updatedBefore time.Time) error
 }
