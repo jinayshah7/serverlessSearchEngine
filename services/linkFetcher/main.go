2a899627c6eb84dea79c9ed292090b7d5dc133d1
jinayshah <jinay.sh99@gmail.com>
2021-10-20 00:30:35 -0400

    get kafka docker compose file, try sarama client

--- services/linkFetcher/main.go
+++ services/linkFetcher/main.go
@@ -1,4 +1,45 @@
 package main
 
-// Find links that haven't been processed yet, using the link repo
-// put the links in a kafka topic
+import (
+	"errors"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	//brokers := os.Getenv("KAFKA_BROKERS")
+
+	err := connectKafka()
+	if err != nil {
+		log.Fatalln("Failed to start Sarama producer:", err)
+	}
+}
+
+func connectKafka() error {
+
+	brokers := "localhost:29092,localhost:39092"
+	sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
+
+	brokerList := strings.Split(brokers, ",")
+	log.Printf("Kafka brokers: %s", strings.Join(brokerList, ", "))
+
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForLocal
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.Producer.Flush.Frequency = 500 * time.Millisecond
+	producer, err := sarama.NewAsyncProducer(brokerList, config)
+
+	if err != nil {
+		return errors.New("could not start Sarama client")
+	}
+
+	go func() {
+		for err := range producer.Errors() {
+			log.Println("Failed to write access log entry:", err)
+		}
+	}()
+}
